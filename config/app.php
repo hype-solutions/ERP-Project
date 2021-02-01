1a9ac285ca1dc908f7048d9f78f7255233b450c9
Ahmed Saleh <whhw47@hotmail.com>
2021-02-01 18:25:44 +0200

    installing barcode package

--- config/app.php
+++ config/app.php
@@ -165,6 +165,9 @@ return [
         /*
          * Package Service Providers...
          */
+        Milon\Barcode\BarcodeServiceProvider::class,
+
+
 
         /*
          * Application Service Providers...
@@ -227,7 +230,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        //Barcode
+        'DNS1D' => Milon\Barcode\Facades\DNS1DFacade::class,
+        'DNS2D' => Milon\Barcode\Facades\DNS2DFacade::class,
     ],
 
 ];
