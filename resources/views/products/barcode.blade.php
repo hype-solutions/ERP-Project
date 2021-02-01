d3f2004da41e43c3ff220846c19296243e71c9df
Ahmed Saleh <whhw47@hotmail.com>
2021-02-01 19:09:46 +0200

    making product barcode work 1.2

--- resources/views/products/barcode.blade.php
+++ resources/views/products/barcode.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>طباعة باركود</title>
 </head>
 <body onload="window.print()">
     @for ($i = 0; $i < $qty; $i++)
