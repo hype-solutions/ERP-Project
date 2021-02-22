e36cee98151564d1c2ac4b1bb14e63706d6bc99f
Ahmed Saleh <whhw47@hotmail.com>
2021-02-22 04:16:38 +0200

    fixing file upload 1.6

--- config/filesystems.php
+++ config/filesystems.php
@@ -80,8 +80,8 @@ return [
 
     'links' => [
         // env('STORAGE_CUSTOM_PATH') => storage_path('app/public'),
-        // base_path(env('STORAGE_CUSTOM_PATH')) => storage_path('app/public'),
-        public_path('storage') => storage_path('app/public'),
+        base_path(env('STORAGE_CUSTOM_PATH')) => storage_path('app/public'),
+        // public_path('storage') => storage_path('app/public'),
     ],
 
 ];
--- routes/web.php
+++ routes/web.php
@@ -482,7 +482,7 @@ Route::get('/shit', [HomeController::class, 'shit'])->name('shit');
 Route::get('/generate', function (){
     // \Illuminate\Support\Facades\Artisan::call('storage:link');
     // app('files')->link(storage_path('app/public'), public_path('storage'));
-    app('files')->link(storage_path('app/public'), '/home2/qzojqlmy/public_html/mygesture/erp/demo/public');
+    // app('files')->link(storage_path('app/public'), '/home2/qzojqlmy/public_html/mygesture/erp/demo/public');
     // Artisan::call('storage:link', [] );
     echo public_path('storage');
 });
