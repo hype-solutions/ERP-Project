7fbae911ddc7dfc40d20d1368976a1dddc2405c7
Ahmed Saleh <whhw47@hotmail.com>
2021-02-22 03:08:38 +0200

    fixing file upload 0.4

--- config/filesystems.php
+++ config/filesystems.php
@@ -79,8 +79,8 @@ return [
     */
 
     'links' => [
-        // public_path(env('STORAGE_CUSTOM_PATH')) => storage_path('app/public'),
-        public_path('storage') => storage_path('app/public'),
+        public_path(env('STORAGE_CUSTOM_PATH')) => storage_path('app/public'),
+        // public_path('storage') => storage_path('app/public'),
     ],
 
 ];
