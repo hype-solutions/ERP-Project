4d5fadd14c3f37d4d28c38c5a8b464fc9a432929
AhmedSaleh0 <whhw47@hotmail.com>
2021-03-21 18:25:51 +0200

    general fixes 213 1.5

--- resources/views/invoices_price_quotations/profile.blade.php
+++ resources/views/invoices_price_quotations/profile.blade.php
@@ -186,6 +186,7 @@
                             <td>الخصم (النسبة)</td>
                             <td class="text-right">
                                 [{{$invoice->discount_percentage}} %]
+                                @if(is_float(($invoice->discount_percentage / 100) * $subtotal)) *** @endif
                                 {{round(($invoice->discount_percentage / 100) * $subtotal)}} ج.م
                             </td>
                           </tr>
@@ -201,7 +202,6 @@
                             <td>الضريبة</td>
                             <td class="text-right">
                                 [{{$invoice->quotation_tax}} %]
-                                @if(is_float(($invoice->discount_percentage / 100) * $subtotal)) *** @endif
                                 {{round(($invoice->quotation_tax / 100) * $subtotal)}}  ج.م</td>
                           </tr>
                           @endif
