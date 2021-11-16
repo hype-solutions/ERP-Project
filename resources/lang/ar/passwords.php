74d21c9f72ab08a5e5bc4a5fe80a25d3cc6ca7bd
ahmed <ahmed@convident.nl>
2021-11-16 21:15:03 +0200

    working on password reset 0.2

---
+++ resources/lang/ar/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'تم تغيير كلمة المرور بنجاح',
+    'sent' => 'أرسلنا اليك رابط لتغيير كلمة المرور',
+    'throttled' => 'برجاء الإنتظار قبل إعادة المحاولة',
+    'token' => 'This password reset token is invalid.',
+    'user' => "هذا البريد الإلكتروني غير مسجل لدينا",
+
+];
