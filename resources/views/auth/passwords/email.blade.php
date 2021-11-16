ce6fe35870ff3a3d62c3ec5c1024eb3e52c54ffd
ahmed <ahmed@convident.nl>
2021-11-16 21:45:05 +0200

    working on password reset 0.3

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -46,6 +46,18 @@
                 </ul>
             </div>
 
+            @if (session('status'))
+            <div class="alert alert-icon-left alert-success alert-dismissible mb-2" role="alert">
+                <span class="alert-icon"><i class="la la-thumbs-o-down"></i></span>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+                <strong>{{ session('status') }}</strong>
+
+            </div>
+
+                    @endif
+
         @endif
             <form method="POST" class="form-horizontal form-simple" action="{{ route('password.email') }}">
               @csrf
