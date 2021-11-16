393a0984c7a56a31e4f61a1bda703c63edec8047
ahmed <ahmed@convident.nl>
2021-11-16 23:16:19 +0200

    working on password reset 1.6

--- resources/views/users/list.blade.php
+++ resources/views/users/list.blade.php
@@ -79,7 +79,14 @@
                             </button>
                             <strong>تم بنجاح!</strong> حذف بيانات مستخدم
                         </div>
-
+                    @elseif (session()->get('success') == 'User Deleted')
+                    <div class="alert alert-icon-left alert-success alert-dismissible mb-2" role="alert">
+                        <span class="alert-icon"><i class="la la-thumbs-o-up"></i></span>
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                        <strong>تم بنجاح!</strong> إرسال رابط لتغيير كلمة المرور للمستخدم
+                    </div>
                     @endif
                 @endif
                 {{-- <div class="users-list-filter px-1">
