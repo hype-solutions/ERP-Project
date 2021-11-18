14cf2102739fe651755acd8b7020b3b4de565386
ahmed <ahmed@convident.nl>
2021-11-18 16:25:22 +0200

    fix error on invoice print

--- resources/views/invoices/print.blade.php
+++ resources/views/invoices/print.blade.php
@@ -70,7 +70,7 @@
                     <p class="text-muted">بيانات الجهه</p>
                   <ul class="px-0 list-unstyled">
                     <li class="text-bold-800">{{$invoice->customer->customer_name}} @if($invoice->customer->parent)
-                        - {{$customer->parent->customer_name}}
+                        - {{$invoice->customer->parent->customer_name}}
                         @endif</li>
                     <li>موبايل: {{ $invoice->customer->customer_mobile }}</li>
                     <li>
