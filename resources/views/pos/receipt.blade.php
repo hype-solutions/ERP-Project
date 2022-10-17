36e4341a81aecd5e7b47922dc16b84fd7fad9c53
Ahmed Saleh <36682441+AhmedSaleh0@users.noreply.github.com>
2022-10-17 17:16:23 +0200

    Development (#20)
    * fix issue with branches (#9)
    
    * EP-105 <now after editing product the page redirected to view the product , I fix an error for the unique code in the edit page it become unique except for this record > (#7)
    
    * adding font size in percentage (#8)
    
    Co-authored-by: Hamid-Balabel <58477083+Hamid-Balabel@users.noreply.github.com>
    
    * most ordered by customer (#13)
    
    * EP-68 <now the buttons in the profile is fixed> (#12)
    
    * EP-67 <now the buttons on the table are responsive> (#11)
    
    * main branch selected on pos if no more branches (#16)
    
    * fixed amount discount appears on reciept (#19)
    
    * Ep 136 purchases orders, عند اضافه اكتر من صنف ترتيب الجدول بيضرب (#18)
    
    * EP-136 <the table now fixed >
    
    * EP-136 <the table now fixed >
    
    Co-authored-by: Hamid-Balabel <58477083+Hamid-Balabel@users.noreply.github.com>

--- resources/views/pos/receipt.blade.php
+++ resources/views/pos/receipt.blade.php
@@ -1,110 +1,142 @@
-
 <html>
-    <head>
-        <title>فاتورة بيع سريع رقم #{{$sessionId}}</title>
+
+<head>
+    <title>فاتورة بيع سريع رقم #{{ $sessionId }}</title>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Cairo&display=swap');
-     @font-face {
-        font-family: 'Cairo', sans-serif;
-    }
-    body {
-        font-family: 'Cairo', sans-serif;
-        font-style: normal;
 
-      }
-      *{
-          direction:rtl;
-          font-size: 15px;
-          font-weight: bold;
+        @font-face {
+            font-family: 'Cairo', sans-serif;
+        }
 
-      }
-        @page
-            {
-                size: auto;   /* auto is the current printer page size */
-                margin: 0mm;  /* this affects the margin in the printer settings */
-            }
-            table {
-        border-collapse: collapse;
-    }
+        body {
+            font-family: 'Cairo', sans-serif;
+            font-style: normal;
 
-    .t1 table, th, td {
-        border: 1px dashed black;
-    }
+        }
 
-    th {
-        height: 50px;
-    }
-      </style>
+        * {
+            direction: rtl;
+            font-size: 15px;
+            font-weight: bold;
 
-    </head>
-     <body onload="window.print()">
-         <center>
-    <img src="{{asset($logo)}}" style="width:200px" height="80px"/></br>
+        }
+
+        @page {
+            size: auto;
+            /* auto is the current printer page size */
+            margin: 0mm;
+            /* this affects the margin in the printer settings */
+        }
+
+        table {
+            border-collapse: collapse;
+        }
+
+        .t1 table,
+        th,
+        td {
+            border: 1px dashed black;
+        }
+
+        th {
+            height: 50px;
+        }
+    </style>
+
+</head>
+
+<body onload="window.print()">
+    <center>
+        <img src="{{ asset($logo) }}" style="width:200px" height="80px" /></br>
     </center>
-     <table border="1" width="95%">
-    <tr>
-    <td align = 'center'><b>رقم الفاتورة</b></td>
-    <td align = 'center'>{{$sessionId}}</td>
-    </tr>
-    <tr>
-        <td align = 'center'><b>وقت الفتح</b></td>
-        <td align = 'center'>{{$currentSession->created_at}}</td>
+    <table border="1" width="95%">
+        <tr>
+            <td align='center'><b>رقم الفاتورة</b></td>
+            <td align='center'>{{ $sessionId }}</td>
         </tr>
-    <tr>
-    <td align = 'center'><b>وقت البيع</b></td>
-    <td align = 'center'>{{$currentSession->sold_when}}</td>
-    </tr>
-    <tr>
-    <td align = 'center'><b>الكاشير</b></td>
-    <td align = 'center'>{{$currentSession->sell_user->username}}</td>
-    </tr>
+        <tr>
+            <td align='center'><b>وقت الفتح</b></td>
+            <td align='center'>{{ $currentSession->created_at }}</td>
+        </tr>
+        <tr>
+            <td align='center'><b>وقت البيع</b></td>
+            <td align='center'>{{ $currentSession->sold_when }}</td>
+        </tr>
+        <tr>
+            <td align='center'><b>الكاشير</b></td>
+            <td align='center'>{{ $currentSession->sell_user->username }}</td>
+        </tr>
     </table>
-     <br/>
-     <table border="1" width="95%">
-    </tr>
-    <tr>
-    <th>الوصف</th>
-    <th>السعر</th>
-    <th>الكمية</th>
-    <th>الإجمالي</th>
-    </tr>
+    <br />
+    <table border="1" width="95%">
+        </tr>
+        <tr>
+            <th>الوصف</th>
+            <th>السعر</th>
+            <th>الكمية</th>
+            <th>الإجمالي</th>
+        </tr>
 
-    <tr>
-            <span style="display: none">{{$subtotal = 0}}</span>
-@foreach($currentCart as $item)
-    <td align='center'>{{$item->product_name}}</b></td>
-            <td align='center'>{{$item->product_price}}</td>
-        <td align='center'>{{$item->product_qty}}</td>
-        <td align='center'>{{$item->product_price * $item->product_qty}}</td>
-        <span style="display: none">{{$subtotal += $item->product_price * $item->product_qty}}</span>
-    </tr>
-@endforeach
+        <tr>
+            <span style="display: none">{{ $subtotal = 0 }}</span>
+            @foreach ($currentCart as $item)
+                <td align='center'>{{ $item->product_name }}</b></td>
+                <td align='center'>{{ $item->product_price }}</td>
+                <td align='center'>{{ $item->product_qty }}</td>
+                <td align='center'>{{ $item->product_price * $item->product_qty }}</td>
+                <span style="display: none">{{ $subtotal += $item->product_price * $item->product_qty }}</span>
+        </tr>
+        @endforeach
 
-                </table>
-         <table border="1" width="95%">
-
+    </table>
+    <table border="1" width="95%">
+        @if ($currentSession->discount_percentage || $currentSession->discount_amount)
             <tr>
-                <td align = 'center'><b>قبل الخصم</b></td>
-                <td align = 'center'><b><font color='red'>{{$subtotal}}</font></b></td>
-                <td align = 'center'><b>جنية</b></td>
-                </tr>
-                <tr>
-                <td align = 'center'><b>الخصم</b></td>
-                <td align = 'center'><b><font color='red'>{{$currentSession->discount_percentage}}</font></b></td>
-                <td align = 'center'><b>%</b></td>
-                </tr>
-
-                <tr>
-                <td align = 'center'><h3>الإجمالي</h3></td>
-                <td align = 'center'><h3><font color='black'>{{$currentSession->total}}</font></h3></td>
-                <td align = 'center'><h3>جنية</h3></td>
-                </tr>
+                <td align='center'><b>قبل الخصم</b></td>
+                <td align='center'><b>
+                        <font color='red'>{{ $subtotal }}</font>
+                    </b></td>
+                <td align='center'><b>جنية</b></td>
+            </tr>
+        @endif
+        @if ($currentSession->discount_percentage)
+            <tr>
+                <td align='center'><b>الخصم</b></td>
+                <td align='center'><b>
+                        <font color='red'>{{ $currentSession->discount_percentage }}</font>
+                    </b></td>
+                <td align='center'><b>%</b></td>
+            </tr>
+        @endif
+        @if ($currentSession->discount_amount)
+            <tr>
+                <td align='center'><b>الخصم</b></td>
+                <td align='center'><b>
+                        <font color='red'>{{ $currentSession->discount_amount }}</font>
+                    </b></td>
+                <td align='center'><b>جنية</b></td>
+            </tr>
+        @endif
+        <tr>
+            <td align='center'>
+                <h3>الإجمالي</h3>
+            </td>
+            <td align='center'>
+                <h3>
+                    <font color='black'>{{ $currentSession->total }}</font>
+                </h3>
+            </td>
+            <td align='center'>
+                <h3>جنية</h3>
+            </td>
+        </tr>
 
     </table>
-    <br/>
-     <table border="1" width="95%">
+    <br />
+    <table border="1" width="95%">
 
-     {{-- <tr>
+        {{-- <tr>
          <td><b> ملاحظات</b></td>
         <td> </td>
     </tr>
@@ -115,6 +147,7 @@
 
     </table>
     {{-- <p style="font-size: 10px;">تاريخ الطباعة: 10-01-2021 05:36:46 PM</p> --}}
-        </font>
-    </body>
-    </html>
+    </font>
+</body>
+
+</html>
