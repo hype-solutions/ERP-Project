e1fc929d8ca4db0e1f0a3903cf655fcf8f6a3b6d
Hamid-Balabel <58477083+Hamid-Balabel@users.noreply.github.com>
2022-10-22 02:50:16 +0200

    Ep 70 on displaying one customer we may make the upper buttons clickable with the nav items below (#14)
    * Update profile.blade.php
    
    * EP-70 <trying to make the upper buttons clickable>

--- resources/views/customers/profile.blade.php
+++ resources/views/customers/profile.blade.php
@@ -240,7 +240,7 @@
                                 <div class="col-xl-6 col-lg-12 mb-1">
                                     <div class="form-group text-center">
                                         <!-- Floating Outline button with text -->
-                                        <button type="button" class="btn btn-float btn-outline-cyan"
+                                        <a type="button" class="btn btn-float btn-outline-cyan" id="invoiceNav"
                                             style="cursor: context-menu"><i
                                                 class="">{{ $customerInvoicesCount + $customerPosSalesCount }}</i><span>عدد
                                                 فواتير
@@ -248,12 +248,12 @@
                                             <br>
                                             <small>{{ $customerPosSalesCount }} بيع سريع - {{ $customerInvoicesCount }}
                                                 فواتير مبيعات</small>
-                                        </button>
+                                            </a>
                                         <button type="button" class="btn btn-float btn-float-lg btn-outline-pink"
                                             style="cursor: context-menu"><i
                                                 class="">{{ $customerInvoicesSum }} ج,م</i><span>إجمالي
                                                 المبالغ من الفواتير</span></button>
-                                        <button type="button" class="btn btn-float btn-outline-cyan"
+                                        <button type="button" class="btn btn-float btn-outline-cyan" id="pQuotationNav"
                                             style="cursor: context-menu"><i
                                                 class="">{{ $customerPriceQuotationCount }}</i><span>عدد
                                                 عروض
@@ -822,6 +822,7 @@
     <script src="{{ asset('theme/app-assets/vendors/js/tables/pdfmake.min.js') }}"></script>
     <script src="{{ asset('theme/app-assets/vendors/js/tables/vfs_fonts.js') }}"></script>
     <script src="{{ asset('theme/app-assets/vendors/js/tables/buttons.html5.min.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- END: Page Vendor JS-->
     <script>
         $("#linked").DataTable({
@@ -1029,6 +1030,47 @@
                 }
             ]
         });
+    </script>
+
+    <script>
+
+        $("#invoiceNav").on('click',function(){
+            $('.nav-link').removeClass('active');
+            $("#active-tab32").addClass('active');
+            
+            if($("#active-tab32").hasClass('active')){
+                
+                // $('#active-tab32').trigger('click');
+                // $('#active-tab32').click();?
+                // $("#active32").siblings().css('display','none');
+                // $("#active32").css('display','block');
+
+            } else{
+                
+            }
+        });
+        
+        // document.getElementById("invoiceNav").onclick = evt => {
+
+        //     simulateClick()
+        // }
+
+        // function simulateClick() {
+
+        //     var evt = new MouseEvent("click");
+
+        //     var cb = document.getElementById("#active-tab32");
+        //     var canceled = !cb.dispatchEvent(evt);
+
+        //     if (canceled) {
+        //         return canceled;
+        //     } else {
+        //         // None of the handlers called preventDefault
+        //         return cb.dispatchEvent(evt);
+        //     }
+        // }
+
+        
     </script>
 
 
