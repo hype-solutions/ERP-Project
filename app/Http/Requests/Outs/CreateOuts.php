ed65deb605f876e75010bd5d6112d201c276e1c4
Ahmed Saleh <36682441+AhmedSaleh0@users.noreply.github.com>
2022-10-27 19:53:53 +0200

    added validation request for outs (#67)

---
+++ app/Http/Requests/Outs/CreateOuts.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests\Outs;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateOuts extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'cat_id' => 'nullable',
+            'entity_id' => 'nullable',
+            'safe_id' => 'required',
+            'date' => 'required',
+            'amount' => 'required|numeric',
+            'notes' => 'nullable',
+        ];
+    }
+
+
+    /**
+     * define the error messages for each input.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'safe_id.required' => 'برجاء إختيار خزنة',
+            'date.required' => 'برجاء إدخال تاريخ',
+            'amount.required' => 'برجاء إدخال المبلغ',
+            'amount.numeric' => 'خانه المبلغ تقبل الأرقام فقط',
+        ];
+    }
+}
