5f4f86edb595297933eb2809d86ee4faa41bf7b8
Hamid-Balabel <58477083+Hamid-Balabel@users.noreply.github.com>
2022-10-31 22:12:29 +0200

    EP-37 <taxes calculated after the discount>

--- resources/views/purchases_orders/add.blade.php
+++ resources/views/purchases_orders/add.blade.php
@@ -485,11 +485,13 @@
             }
 
             var currentInvoiceTotal = $("#total_after_all").text();
-            var descountAmount = $("#discount_amount").text();
-            currentInvoiceTotal = parseInt(currentInvoiceTotal) - parseInt(descountAmount);
-            
-            var newInvoiceTotal = currentInvoiceTotal - (currentInvoiceTotal * (newTax / 100));
-            var taxAmount = currentInvoiceTotal - newInvoiceTotal;
+            var getDiscountAmount_1 = $('#discount_percentage_amount').text();
+            var getDiscountAmount_2 = $('#discount_amount').text();
+
+            currentInvoiceTotal = parseInt(currentInvoiceTotal) - parseInt(getDiscountAmount_1) - parseInt(getDiscountAmount_2);
+
+            var newInvoiceTotal = currentInvoiceTotal + (currentInvoiceTotal * (newTax / 100));
+            var taxAmount = newInvoiceTotal - currentInvoiceTotal ;
             taxAmount = Math.round(taxAmount);
             $('#tax_amount').text(taxAmount);
 
