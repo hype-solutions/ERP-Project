f29a920145186e993b4ef56f4a1b6497e47d2172
Ahmed Saleh <whhw47@hotmail.com>
2023-09-05 15:54:04 +0300

    move buttons to left side

--- resources/views/customers/list.blade.php
+++ resources/views/customers/list.blade.php
@@ -76,46 +76,7 @@
                         </div>
                     @endif
                 @endif
-                {{-- <div class="users-list-filter px-1">
-  <form>
-      <div class="row border border-light rounded py-2 mb-2">
-          <div class="col-12 col-sm-6 col-lg-3">
-              <label for="users-list-verified">Verified</label>
-              <fieldset class="form-group">
-                  <select class="form-control" id="users-list-verified">
-                      <option value="">Any</option>
-                      <option value="Yes">Yes</option>
-                      <option value="No">No</option>
-                  </select>
-              </fieldset>
-          </div>
-          <div class="col-12 col-sm-6 col-lg-3">
-              <label for="users-list-role">Role</label>
-              <fieldset class="form-group">
-                  <select class="form-control" id="users-list-role">
-                      <option value="">Any</option>
-                      <option value="User">User</option>
-                      <option value="Staff">Staff</option>
-                  </select>
-              </fieldset>
-          </div>
-          <div class="col-12 col-sm-6 col-lg-3">
-              <label for="users-list-status">Status</label>
-              <fieldset class="form-group">
-                  <select class="form-control" id="users-list-status">
-                      <option value="">Any</option>
-                      <option value="Active">Active</option>
-                      <option value="Close">Close</option>
-                      <option value="Banned">Banned</option>
-                  </select>
-              </fieldset>
-          </div>
-          <div class="col-12 col-sm-6 col-lg-3 d-flex align-items-center">
-              <button class="btn btn-block btn-primary glow">Show</button>
-          </div>
-      </div>
-  </form>
-</div> --}}
+
                 <div class="users-list-table">
                     <div class="card">
                         <div class="card-content">
